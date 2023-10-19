9e67bba5f532b8f76ee2ac761d1aa8dfdcdfd75e
Wildaniabs <2200004@ipisa.edu.do>
2023-10-19 15:44:42 -0400

    yop

---
+++ src/pasantia_proyect/controlador/MyConnection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasantia_proyect.controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author A19B59953
+ */
+public class MyConnection {
+    public static Connection getConnection(){
+    Connection con = null;
+    
+    try{
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Proveedor","root","irisalbanaia13");
+    } catch (ClassNotFoundException | SQLException ex){
+        System.out.println(ex.getMessage());
+    }
+    return con;
+    
+    }
+}
